601229f2056995cd0688ec4be43ba6142f207d40
sharowyeh <yuyu.yei@hotmail.com>
2019-09-20 19:23:30 +0800

    Update example for arc line

---
+++ WPFExample/StringToDoubleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFExample
+{
+    public class StringToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string && double.TryParse((string)value, out double result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+    }
+}
